9c7f04592e7f9e5501e93b8c456f3766978b2f8c
Matulan Mahenthra <Matu_25@hotmail.com>
2022-08-10 10:18:03 +0100

    update2

--- routes/cars.routes.js
+++ routes/cars.routes.js
@@ -6,54 +6,6 @@ router.get("/car", (req, res, next) => {
   res.render("car");
 });
 
-router.post("/car/create", (req, res, next) => {
-  const carDetails = {
-    vehiclename: req.body.vehiclename,
-    vehicletype: req.body.vehicletype,
-    price: req.body.price,
-    size: req.body.size,
-    passengers: req.body.passengers,
-    picture: req.body.picture,
-    description: req.body.description,
-    engine: req.body.engine,
-    bhp: req.body.bhp,
-    zerotohundred: req.body.zerotohundred,
-    topspeed: req.body.topspeed,
-  };
-
-  Car.create(carDetails)
-    .then(() => {
-      res.redirect("/car");
-    })
-    .catch((error) => {
-      console.log("Error creating boat in the DB", error);
-      next(error);
-    });
-});
-
-router.get("/car-list", (req, res, next) => {
-  Car.find()
-    .then((allcar) => {
-      res.render("car-list", { car: allcar });
-    })
-    .catch((err) => {
-      console.log("Error while creating the car");
-      next(err);
-    });
-});
-
-router.get("/car/:carId", (req, res, next) => {
-  const { carId } = req.params;
-
-  Car.findById(carId)
-    .then((car) => {
-      res.render("car-details", { car });
-    })
-    .catch((err) => {
-      console.log(err);
-      next(err);
-    });
-});
 
 router.post("/car/create", (req, res, next) => {
     const carDetails = {
